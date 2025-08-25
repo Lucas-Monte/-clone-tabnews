eedf1e03700653b5872b1fbe717fb643f467ab85
Lucas Antonio Monteiro <lucasantoniomonteiro1@gmail.com>
2025-08-25 00:45:50 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home() {
+    return <h1>Ana Laura, eu te amo muitooo. Se você me ama tambem, ta me devendo um beijão.</h1>
+}
+
+export default Home; //export padrão, fazendo com que um componete chama outro, formando o que tiver na nossa pagina
+
