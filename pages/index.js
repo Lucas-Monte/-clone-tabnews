03d56749cb86b2b1ae802f68cd847c9f27f26310
Lucas Antonio Monteiro <lucasantoniomonteiro1@gmail.com>
2025-08-30 17:27:46 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Ana Laura, eu te amo muitooo. Se você me ama tambem, ta me devendo um beijão.</h1>
+    return <h1>Ana Laura, eu te amo muitooo. Se você me ama tambem, ta me devendo um beijão.❤️😘💖</h1>
 }
 
 export default Home; //export padrão, fazendo com que um componete chama outro, formando o que tiver na nossa pagina
