723af2b909b52d889c0b6a33c1c5fb4f441bca21
Lucas Antonio Monteiro <lucasantoniomonteiro1@gmail.com>
2025-09-01 01:05:03 +0000

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,4 @@ function Home() {
   );
 }
 
-function teste() {
-  console.log(teste);
-}
-
-function teste2() {
-  console.log("identação errada");
-}
-
 export default Home; //export padrão, fazendo com que um componete chama outro, formando o que tiver na nossa pagina
