6a5fac2903dc9f0d8f783d9f26d68764d187620d
Lucas Antonio Monteiro <lucasantoniomonteiro1@gmail.com>
2025-09-13 01:11:33 +0000

    altera resposta status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,5 @@
 function status(request, response) {
-  response.status(200).json({ chave: "São acima da média" });
+  response.status(200).json({ chave: "Sai daqui Curioso" });
 }
 
 export default status;
